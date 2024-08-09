5d187cb95c8dbe7b1aaa8a6134099629cbb3b6fa
Hermano Flávio de Moura <hermanoudi@gmail.com>
2024-08-09 10:28:33 -0300

    test: :safety_vest: adding new tests

--- tests/constants.py
+++ tests/constants.py
@@ -1,3 +1,3 @@
 import os
 TEST_PATH = os.path.dirname(__file__)
-PEOPLE_FILE = os.path.join(TEST_PATH, 'assets/extrato.csv')
+EXTRACT_FILE = os.path.join(TEST_PATH, 'assets/extrato.csv')
--- tests/test_load.py
+++ tests/test_load.py
@@ -2,44 +2,19 @@ import os
 import pytest
 import uuid
 from mouracx.core import load
-from .constants import PEOPLE_FILE
-
-def setup_module():
-    print("roda antes dos testes desse modulo\n")
-
-def teardown_module():
-    print("roda após os testes desse modulo\n")
-
-
-
-@pytest.fixture(scope="function", autouse=True)
-def create_new_file(tmpdir):
-    file_ = tmpdir.join("new_file.txt")
-    file_.write("isso é sujeira..")
-    yield
-    file_.remove()
-
-@pytest.mark.unit
-@pytest.mark.high
-def test_load(request):
-    """Test Load Funcition"""
-
-    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
-    request.addfinalizer(lambda: os.unlink(filepath))
-
-    with open(filepath, "w") as file_:
-        file_.write("dados uteis somente para o teste")
-
-    assert len(load(PEOPLE_FILE)) > 0
-    assert load(PEOPLE_FILE)[0][0] == '3'
+from .constants import EXTRACT_FILE
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load2():
+def test_load_positive_has_extract_filled(request):
     """Test Load Funcition"""
-    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
-        file_.write("dados uteis somente para o teste")
+    assert len(load(EXTRACT_FILE)) > 0
 
-    assert len(load(PEOPLE_FILE)) > 0
-    assert load(PEOPLE_FILE)[0][0] == '3'
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_negative_has_extract_filled(request):
+    """Test Load Funcition"""
+    assert len(load(EXTRACT_FILE)) is None
